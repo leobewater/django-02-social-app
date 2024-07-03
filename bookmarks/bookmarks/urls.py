b2bb9c6ff7ac951dc1c0cbccda683cadad53481d
leobewater <mmbee888@gmail.com>
2024-07-03 08:58:45 -0400

    register profile to admin

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -5,3 +7,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
 ]
+
+# Debug mode
+# The static() helper function is suitable for development but not for production use. Django is very inefficient at serving static files. Never serve your static files with Django in a production environment.
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
