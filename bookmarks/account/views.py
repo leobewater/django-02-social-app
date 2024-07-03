a028cf8a783a293da42f1db9e523b73618ecd560
leobewater <mmbee888@gmail.com>
2024-07-03 09:13:47 -0400

    add edit user form

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -7,51 +7,6 @@ from .forms import LoginForm, ProfileEditForm, UserEditForm, UserRegistrationFor
 from .models import Profile
 
 
-@login_required
-def edit(request):
-    if request.method == 'POST':
-        user_form = UserEditForm(
-            instance=request.user,
-            data=request.POST
-        )
-
-        profile_form = ProfileEditForm(
-            instance=request.user.profile,
-            data=request.POST,
-            files=request.FILES
-        )
-
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-    else:
-        user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.user)
-        return render(request, 'account/edit.html', {
-            'user_form': user_form,
-            'profile_form': profile_form
-        })
-
-
-def register(request):
-    if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-
-        if user_form.is_valid():
-            new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password'])
-            new_user.save()
-
-            # Create user profile associated with the new_user
-            Profile.objects.create(user=new_user)
-
-            return render(request, 'account/register_done.html', {'new_user': new_user})
-    else:
-        user_form = UserRegistrationForm()
-
-    return render(request, 'account/register.html', {'user_form': user_form})
-
-
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -80,3 +35,49 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+
+            # Create user profile associated with the new_user
+            Profile.objects.create(user=new_user)
+
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+
+    return render(request, 'account/register.html', {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(
+            instance=request.user,
+            data=request.POST
+        )
+
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user)
+
+    return render(request, 'account/edit.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
