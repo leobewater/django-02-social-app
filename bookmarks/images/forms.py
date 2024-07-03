4fb060948f307281cddea65c24a2fc31dd53fc09
leobewater <mmbee888@gmail.com>
2024-07-03 15:21:18 -0400

    add image create url

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -11,7 +11,7 @@ from .models import Image
 class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
-        fields = ['title', 'ur', 'description']
+        fields = ['title', 'url', 'description']
         widget = {'url': forms.HiddenInput}
 
     def clean_url(self):
