f624c6241e4e2fdb41bc68654e5ad3af6caff35e
leobewater <mmbee888@gmail.com>
2024-07-03 01:44:09 -0400

    add dashboard

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,7 +1,15 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # our custom login
+    # path('login/', views.user_login, name='login'),
+
+    # using django auth views
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('', views.dashboard, name='dashboard'),
 ]
