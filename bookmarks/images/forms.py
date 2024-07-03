4b2a87eef8e999cf9383fd4670079a90992cd48b
leobewater <mmbee888@gmail.com>
2024-07-03 15:12:11 -0400

    override image save method

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,4 +1,9 @@
+from typing import Any
+
+import requests
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 from .models import Image
 
@@ -18,3 +23,23 @@ class ImageCreateForm(forms.ModelForm):
                 'The given URL does not match valid image extensions.'
             )
         return url
+
+    # Override save
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+
+        # download image from the given URL
+        response = requests.get(image_url)
+        image.image.save(
+            image_name,
+            ContentFile(response.content),
+            save=False
+        )
+
+        if commit:
+            image.save()
+        return image
