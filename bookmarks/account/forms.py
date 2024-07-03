1dfa2100642ef58f665c51e237a95ac019f5051a
leobewater <mmbee888@gmail.com>
2024-07-03 09:01:04 -0400

    add user edit and user profile form classes

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,6 +1,20 @@
 from django import forms
 from django.contrib.auth import authenticate, get_user_model, login
 
+from .models import Profile
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email']
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
