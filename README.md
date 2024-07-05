168de5623ade9448a0708eea1bf90ea8b14c4272
leobewater <mmbee888@gmail.com>
2024-07-05 09:52:38 -0400

    add user list and detail page

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ add host to /etc/hosts
 127.0.0.1  mysite.com
 
 > py manage.py runserver_plus --cert-file cert.crt
+
+# browser
+https://mysite.com:8000/
+https://mysite.com:8000/admin/
 ```
--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -12,3 +13,44 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Profile of {self.user.username}'
+
+
+class Contact(models.Model):
+    user_from = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='rel_from_set',
+        on_delete=models.CASCADE
+    )
+
+    user_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='rel_from_to',
+        on_delete=models.CASCADE
+    )
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
+
+
+# Additional fields for the Contact
+# Add following field to User dynamically
+# You use the add_to_class() method of Django models to monkey patch the User model.
+user_model = get_user_model()
+user_model.add_to_class(
+    'following',
+    models.ManyToManyField(
+        'self',
+        through=Contact,
+        related_name='followers',
+        symmetrical=False,
+    ),
+)
