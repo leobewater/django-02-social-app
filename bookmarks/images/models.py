42341b4ddc7d9f4a24be5845f413ee41bd3e1db4
leobewater <mmbee888@gmail.com>
2024-07-06 15:15:20 -0400

    add signals and receiver to update image total likes

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -8,10 +8,10 @@ class Image(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name='images_created',
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
     title = models.CharField(max_length=200)
-    slug = models.CharField(max_length=200, blank=True)
+    slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField(max_length=2000)
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
@@ -21,7 +21,7 @@ class Image(models.Model):
     users_like = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
         related_name='images_liked',
-        blank=True
+        blank=True,
     )
 
     # dedicated field for total like for performance
@@ -41,7 +41,7 @@ class Image(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
-            super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse("images:detail", args=[self.id, self.slug])
