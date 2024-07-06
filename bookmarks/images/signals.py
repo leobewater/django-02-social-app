89e9b86a52eaf5b1d82a41e2ff4889ee178d4417
leobewater <mmbee888@gmail.com>
2024-07-06 14:59:22 -0400

    set up signal for image total likes

---
+++ bookmarks/images/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+
+from .models import Image
+
+
+# Django signals are synchronous and blocking
+# attach the receiver to the m2m_changed and connect to the sender function
+@receiver(m2m_changed, sender=Image.users_like.through)
+def users_like_changed(sender, instance, **kwargs):
+    instance.total_likes = instance.users_like.count()
+    instance.save()
